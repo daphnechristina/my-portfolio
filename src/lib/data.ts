014a7b821e4b0be590905e7fff5637868c6ba1ca
daphnechrisitna <daphne.prem@gmail.com>
2025-06-17 13:12:56 +0530

    Initial Commit

---
+++ src/lib/data.ts
@@ -0,0 +1,177 @@
+export const personalInfo = {
+  name: "P Daphne Christina Ruby",
+  location: "Bengaluru, India",
+  email: "daphne.prem@gmail.com",
+  github: "https://github.com/daphnechristina",
+  linkedin: "https://www.linkedin.com/in/daphne-christina-ruby-098990313/",
+};
+
+{/*export const workExperience = [
+  {
+    company: "Chatbyte GmbH",
+    location: "Remote",
+    position: "Software Engineer",
+    period: "Mar 2024 - Present",
+    achievements: [
+      "Designed and implemented business logic for AWS Lambdas using the Serverless Framework, forming the backbone of the application's functionality and ensuring seamless backend operations.",
+      "Built a comprehensive admin panel for managing content, user data, and analytics.",
+      "Contributed to the development and integration of a scalable CMS for managing blog content and other platform data.",
+      "Reduced database costs by introducing materialized views, optimizing query performance and resource usage.",
+      "Drove the integration of a custom affiliate marketing system, enabling seamless tracking and reporting of referrals and user activities.",
+      "Integrated Text-to-Speech (TTS) services for enhanced user experiences, optimizing both frontend and backend systems.",
+      "Collaborated with cross-functional teams to deliver high-quality features, actively reviewing pull requests to ensure code quality, adherence to standards, and efficient implementation.",
+    ],
+  },
+  {
+    company: "DevCrew",
+    location: "Coimbatore, India",
+    position: "Software Engineer",
+    period: "Mar 2023 - Mar 2024",
+    achievements: [
+      "Developed responsive frontend components using React.js, enhancing user experience and app performance.",
+      "Optimized JavaScript solutions to improve load times and data-fetching efficiency.",
+    ],
+  },
+  {
+    company: "Phoenitags",
+    location: "Coimbatore, India",
+    position: "Software Engineering Intern",
+    period: "Jan 2023 - Mar 2023",
+    achievements: [
+      "Built interactive UIs with React.js, focusing on reusable components and performance optimization.",
+      "Implemented JavaScript-based features like user authentication and real-time updates.",
+    ],
+  },
+];*/}
+export const education = [
+  {
+    institution: "Vellore Institute of Technology",
+    location: "Vellore, India",
+    degree: "B Tech Electronics and Communication Engineering",
+    period: "July 2024 - ",
+    //achievements1: [
+        //   "G20 student delegate",
+    //   "Organized multiple technical workshops and events",
+    //   "Represented my college in multiple national and international hackathons and won.",
+    //],
+    
+    school1: "HAL Public School",
+    location1: "Bengaluru, India",
+    subject1: "93.4% CBSE (PCMB)",
+    period1: "June 2010 - March 2015 and June 2022 - March 2024",
+    achievements: [
+       {
+      text: "Official TedX Speaker",
+      link: "https://youtu.be/HEkcxM5XCmg?si=uuRnAC3MvSxNJa8O"
+    }],
+
+    school2: "Baldwin Girls' High School,",
+    subject2: "92% ICSE",
+    location2: "Bengaluru, India",
+    period2: "June 2015 - March 2022",
+    //achievements2:
+  }
+];
+
+export const skills = {
+  programmingLanguages: [
+    "JavaScript",
+    "Python",
+    "Java"
+  ],
+  frontendDevelopment: [
+    "Nextjs",
+    "Reactjs",
+    "Tailwind CSS",
+    "HTML",
+    "CSS",
+  ],
+  backendDevelopment: ["Nodejs", "Expressjs"],
+  uiUxDesign: ["Figma"]
+  //databaseAndStorage: ["PostgreSQL", "Drizzle (ORM)"],
+  //cloudAndDevOps: ["AWS"],
+  // toolsAndServices: [
+  //   "Clerk (Auth)",
+  //   "Sanity (CMS)",
+  //   "Tinybird (analytics)",
+  //   "Zod",
+  //   "Sentry",
+  //   "Mixpanel",
+  //   "Trigger.dev",
+  // ],
+};
+
+export const projects = [
+  {
+    title: "Shirt Designing with AI",
+    github: "https://github.com/daphnechristina/project_threejs_ai",
+    description: [
+      "Customize your own shirt with a 3D model Display.",
+      "Use AI image generation for your designs.",
+      "Upload your own images for your designs.",
+    ],
+  },
+  //{
+    //title: "Mental Aarog",
+    //github: "https://github.com/rishikesh2003/mental-aarog",
+    //description: [
+      //"A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
+      //"Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
+      //"Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
+      //"Secure storage implemented via web3.storage, IPFS, and FileCoin.",
+      //"Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
+    //],
+  //},
+];
+
+{/*export const awards = [
+  {
+    name: "IEEE YESIST12 Hackathon",
+    issuer: "IEEE",
+    date: "Sep 2022",
+    type: "International",
+    position: "Second Place",
+  },
+  {
+    name: "Prodigi Cognizant Hackathon",
+    issuer: "Cognizant",
+    date: "Feb 2023",
+    type: "National",
+    position: "Second Runner-up",
+  },
+  {
+    name: "Cisco Thingqbator Hackathon",
+    issuer: "Cisco",
+    date: "Jan 2023",
+    type: "National",
+    position: "First Runner-up",
+  },
+  {
+    name: "Innovators Day",
+    issuer: "Sri Manakula Vinayagar Engineering College, Pondicherry",
+    date: "Sep 2022",
+    type: "National",
+    position: "First Prize",
+  },
+  {
+    name: "KG Hackfest'22",
+    issuer: "KGiSL Institute of Technology, Coimbatore",
+    date: "Sep 2022",
+    type: "National",
+    position: "Second Prize",
+  },
+  {
+    name: "Innohacks'22",
+    issuer: "Innogeeks, KIET Group of Institutions, New Delhi",
+    date: "May 2022",
+    type: "National",
+    position: "Second Runner-up",
+  },
+  {
+    name: "Hack @ SKCET",
+    issuer: "Hackclub SKCET, SKCET, Coimbatore",
+    date: "Feb 2022",
+    type: "National",
+    position: "Most Impactful Hack",
+  },
+];*/}
